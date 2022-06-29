f01f53fd9f15d71136acb631d43e3053911af868
Mat-Plumski <97526493+Mat-Plumski@users.noreply.github.com>
2022-06-29 14:08:51 +0100

    Added an incomplete 2nd entry to My Work section

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const navBar = document.getElementById("nav-bar")
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector(".nav-toggle")
+const navLinks = document.querySelectorAll(".nav__link")
+const minimisedImages = document.querySelectorAll(".portfolio__page--image-frame")
 
 
 function stickyNavBar() {
     if(window.pageYOffset === 0){
-        navBar.classList.remove('semi-transparent')
+        navBar.classList.remove("semi-transparent")
     } else if(window.pageYOffset > 0){
-        navBar.classList.add('semi-transparent')
+        navBar.classList.add("semi-transparent")
     }
 }
 
 window.addEventListener("scroll", stickyNavBar)
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open")
+})
 
 navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open")
+    })
+})
+
+minimisedImages.forEach(image => {
+    image.addEventListener("click", () => {
+        image.classList.toggle("portfolio__page--image-frame--fullscreen")
     })
 })
